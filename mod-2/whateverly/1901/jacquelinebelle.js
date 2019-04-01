7281d16c9dd234124f0baa359bf730a4e36b6b2d
Jacqueline Moore <jacquelinebmoore@gmail.com>
2019-04-01 11:49:09 -0600

    I was thinking of an app where you could avoid Will Ferrell. I have a dataset listing his appearances and movies/tv shows and another listing some places he's been seen. I don't know who this would help.

---
+++ mod-2/whateverly/1901/jacquelinebelle.js
@@ -0,0 +1,282 @@
+const appearances = [
+    {
+      "title": "The Lego Movie 2: The Second Part",
+      "year": 2019,
+      "tv": false,
+      "producer": false,
+      "writer": false
+    },
+    {
+      "title": "Drunk History",
+      "year": 2019
+      "tv": true,
+      "episode": ["Are You Afraid of the Drunk?", "Spies"],
+      "producer": true,
+      "writer": false
+    },
+    {
+      "title": "Holmes & Watson",
+      "year": 2018,
+      "tv": false,
+      "producer": true,
+      "writer": false
+    },
+    {
+      "title": "I Love You, America",
+      "year": 2018,
+      "tv": true,
+      "episode": "Bill Burr",
+      "producer": false,
+      "writer": false
+    },
+    {
+      "title": "Saturday Night Live",
+      "year": 2018,
+      "tv": true,
+      "episode": "just avoid",
+      "producer": false,
+      "writer": false
+    },
+    {
+      "title": "Daddy's Home 2",
+      "year": 2017,
+      "tv": false,
+      "producer": true,
+      "writer": false
+    },
+    {
+      "title": "No Activity",
+      "year": 2017,
+      "tv": true,
+      "episode": "just avoid",
+      "producer": true,
+      "writer": false
+    },
+    {
+      "title": "Throwing Shade",
+      "year": 2017,
+      "tv": true,
+      "episode": ""Episode" #1.1",
+      "producer": false,
+      "writer": false
+    },
+    {
+      "title": "The House",
+      "year": 2017,
+      "tv": false,
+      "producer": true,
+      "writer": false
+    },
+    {
+      "title": "Zoolander 2",
+      "year": 2016,
+      "tv": false,
+      "producer": false,
+      "writer": false
+    },
+    {
+      "title": "Daddy's Home",
+      "year": 2015,
+      "tv": false,
+      "producer": true,
+      "writer": false
+    },
+    {
+      "title": "The Last Man on Earth",
+      "year": 2015,
+      "tv": true,
+      "episode": "The Boo",
+      "producer": false,
+      "writer": false
+    },
+    {
+      "title": "Get Hard",
+      "year": 2015,
+      "tv": false,
+      "producer": false,
+      "writer": false
+    },
+    {
+      "title": "The Spoils of Babylon",
+      "year": 2014,
+      "tv": true,
+      "episode": "just avoid",
+      "producer": true,
+      "writer": false
+    },
+    {
+      "title": "The Lego Movie",
+      "year": 2014,
+      "tv": false,
+      "producer": false,
+      "writer": false
+    },
+    {
+      "title": "Anchorman 2: The Legend Continues",
+      "year": 2013,
+      "tv": false,
+      "producer": true,
+      "writer": true
+    },
+    {
+      "title": "30 Rock",
+      "year": 2012,
+      "tv": true,
+      "episode": ["My Whole Life Is Thunder", "The Moms", "Lee Marvin vs Derek Jeter"],
+      "producer": false,
+      "writer": false
+    },
+    {
+      "title": "The Campaign",
+      "year": 2012,
+      "tv": false,
+      "producer": true,
+      "writer": false
+    },
+    {
+      "title": "Eastbound & Down",
+      "year": 2012,
+      "tv": true,
+      "episode": "just avoid",
+      "producer": false,
+      "writer": false
+    },
+    {
+      "title": "Casa de mi Padre",
+      "year": 2012,
+      "tv": false,
+      "producer": true,
+      "writer": false
+    },
+    {
+      "title": "Tim and Eric's Billion Dollar Movie",
+      "year": 2012,
+      "tv": false,
+      "producer": true,
+      "writer", false
+    },
+    {
+      "title": "The Office",
+      "year": 2011,
+      "tv": true,
+      "episode": ["The Inner Circle", "Goodbye, Michael", "Michael's Last Dundies", "Training Day"],
+      "producer": false,
+      "writer": false
+    },
+    {
+      "title": "Funny or Die Presents...",
+      "year": 2011,
+      "tv": true,
+      "episode": [""Episode" 2.3", "The Carpet Brothers"],
+      "producer": true,
+      "writer": true
+    },
+    {
+      "title": "Megamind",
+      "year": 2010,
+      "tv": false,
+      "producer": false,
+      "writer": false
+    },
+    {
+      "title": "Everything Must Go",
+      "year": 2010,
+      "tv": false,
+      "producer": false,
+      "writer": false
+    },
+    {
+      "title": "The Other Guys",
+      "year": 2010,
+      "tv": false,
+      "producer": true,
+      "writer": false
+    },
+    {
+      "title": "Tim and Eric Awesome Show, Great Job",
+      "year": 2010,
+      "tv": true,
+      "episode": "Stuntmen",
+      "producer": false,
+      "writer": false
+    },
+    {
+      "title": "Land of the Lost",
+      "year": 2009,
+      "tv": false,
+      "producer": false,
+      "writer": false
+    },
+    {
+      "title": "Step Brothers",
+      "year": 2008,
+      "tv": false,
+      "producer": true,
+      "writer": true
+    },
+    {
+      "title": "Semi-Pro",
+      "year": 2008,
+      "tv": false,
+      "producer": false,
+      "writer": false
+    }
+  ];
+
+const sightings = [
+    {
+      "city": "Boston",
+      "year": 2017,
+      "place": "Unnamed Italian Restaurant"
+    },
+    {
+      "city": "Chicago",
+      "year": 2013,
+      "place": "Del Frisco's"
+    },
+    {
+      "city": "Covington",
+      "year": 2016,
+      "place": "Bradley’s Real Pit Barbecue"
+    },
+    {
+      "city": "Terre Haute",
+      "year": 2011,
+      "place": "Cracker Barrell",
+      "description": “had some pretty goofy looking clothes on”
+    },
+    {
+      "city": "Atlanta",
+      "year": 2013,
+      "place": "Urban Licks"
+    },
+    {
+      "city": "Coronado",
+      "year": 2012
+    },
+    {
+      "city": "New York "City"",
+      "year": 2011,
+      "place": "5th Ave",
+      "description": "looking pimped out"
+    },
+    {
+      "city": "Inglewood",
+      "year": 2018
+    },
+    {
+      "city": "Orange County",
+      "year": 2018,
+      "place": "Interstate 5"
+    },
+    {
+      "city": "San Diego",
+      "year": 2013,
+      "place": "Lakers game"
+    }
+  ];
+
+module.exports = {
+  appearances,
+  sightings
+}
