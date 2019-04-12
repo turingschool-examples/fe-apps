0551818527f027c4a6c28a002d2ca6dc6e18168b
Jarrett Kong <jarrettkong@me.com>
2019-04-12 11:00:02 -0600

    Added initial dataset structure and two samples

---
+++ mod-2/memoize/1901/jarrettkong.js
@@ -0,0 +1,274 @@
+const lessons = [
+  {
+    "id": 1,
+    "category": "General",
+    "regex": "",
+    "instructions": "Finds exactly what is between the two '/'",
+    "prompt": "Turing",
+    "examples": ["Turing School of Software and Design"],
+    "solutions": ["Turing"]
+  },
+  {
+    "id": 2,
+    "category": "Classes",
+    "regex": "\d",
+    "instructions": "Finds a single occurance of any digit 0-9",
+    "prompt": "find",
+    "examples": ["1234567890"],
+    "solutions": ["1","2","3","4","5","6","7","8","9","0"]
+  },
+  {
+    "id": 3,
+    "category": "",
+    "regex": "",
+    "instructions": "",
+    "prompt": "",
+    "examples": [],
+    "solutions": []
+  },
+  {
+    "id": 4,
+    "category": "",
+    "regex": "",
+    "instructions": "",
+    "prompt": "",
+    "examples": [],
+    "solutions": []
+  },
+  {
+    "id": 5,
+    "category": "",
+    "regex": "",
+    "instructions": "",
+    "prompt": "",
+    "examples": [],
+    "solutions": []
+  },
+  {
+    "id": 6,
+    "category": "",
+    "regex": "",
+    "instructions": "",
+    "prompt": "",
+    "examples": [],
+    "solutions": []
+  },
+  {
+    "id": 7,
+    "category": "",
+    "regex": "",
+    "instructions": "",
+    "prompt": "",
+    "examples": [],
+    "solutions": []
+  },
+  {
+    "id": 8,
+    "category": "",
+    "regex": "",
+    "instructions": "",
+    "prompt": "",
+    "examples": [],
+    "solutions": []
+  },
+  {
+    "id": 9,
+    "category": "",
+    "regex": "",
+    "instructions": "",
+    "prompt": "",
+    "examples": [],
+    "solutions": []
+  },
+  {
+    "id": 10,
+    "category": "",
+    "regex": "",
+    "instructions": "",
+    "prompt": "",
+    "examples": [],
+    "solutions": []
+  },
+  {
+    "id": 11,
+    "category": "",
+    "regex": "",
+    "instructions": "",
+    "prompt": "",
+    "examples": [],
+    "solutions": []
+  },
+  {
+    "id": 12,
+    "category": "",
+    "regex": "",
+    "instructions": "",
+    "prompt": "",
+    "examples": [],
+    "solutions": []
+  },
+  {
+    "id": 13,
+    "category": "",
+    "regex": "",
+    "instructions": "",
+    "prompt": "",
+    "examples": [],
+    "solutions": []
+  },
+  {
+    "id": 14,
+    "category": "",
+    "regex": "",
+    "instructions": "",
+    "prompt": "",
+    "examples": [],
+    "solutions": []
+  },
+  {
+    "id": 15,
+    "category": "",
+    "regex": "",
+    "instructions": "",
+    "prompt": "",
+    "examples": [],
+    "solutions": []
+  },
+  {
+    "id": 16,
+    "category": "",
+    "regex": "",
+    "instructions": "",
+    "prompt": "",
+    "examples": [],
+    "solutions": []
+  },
+  {
+    "id": 17,
+    "category": "",
+    "regex": "",
+    "instructions": "",
+    "prompt": "",
+    "examples": [],
+    "solutions": []
+  },
+  {
+    "id": 18,
+    "category": "",
+    "regex": "",
+    "instructions": "",
+    "prompt": "",
+    "examples": [],
+    "solutions": []
+  },
+  {
+    "id": 19,
+    "category": "",
+    "regex": "",
+    "instructions": "",
+    "prompt": "",
+    "examples": [],
+    "solutions": []
+  },
+  {
+    "id": 20,
+    "category": "",
+    "regex": "",
+    "instructions": "",
+    "prompt": "",
+    "examples": [],
+    "solutions": []
+  },
+  {
+    "id": 21,
+    "category": "",
+    "regex": "",
+    "instructions": "",
+    "prompt": "",
+    "examples": [],
+    "solutions": []
+  },
+  {
+    "id": 22,
+    "category": "",
+    "regex": "",
+    "instructions": "",
+    "prompt": "",
+    "examples": [],
+    "solutions": []
+  },
+  {
+    "id": 23,
+    "category": "",
+    "regex": "",
+    "instructions": "",
+    "prompt": "",
+    "examples": [],
+    "solutions": []
+  },
+  {
+    "id": 24,
+    "category": "",
+    "regex": "",
+    "instructions": "",
+    "prompt": "",
+    "examples": [],
+    "solutions": []
+  },
+  {
+    "id": 25,
+    "category": "",
+    "regex": "",
+    "instructions": "",
+    "prompt": "",
+    "examples": [],
+    "solutions": []
+  },
+  {
+    "id": 26,
+    "category": "",
+    "regex": "",
+    "instructions": "",
+    "prompt": "",
+    "examples": [],
+    "solutions": []
+  },
+  {
+    "id": 27,
+    "category": "",
+    "regex": "",
+    "instructions": "",
+    "prompt": "",
+    "examples": [],
+    "solutions": []
+  },
+  {
+    "id": 28,
+    "category": "",
+    "regex": "",
+    "instructions": "",
+    "prompt": "",
+    "examples": [],
+    "solutions": []
+  },
+  {
+    "id": 29,
+    "category": "",
+    "regex": "",
+    "instructions": "",
+    "prompt": "",
+    "examples": [],
+    "solutions": []
+  },
+  {
+    "id": 30,
+    "category": "",
+    "regex": "",
+    "instructions": "",
+    "prompt": "",
+    "examples": [],
+    "solutions": []
+  }
+]
+
+export default lessons;
