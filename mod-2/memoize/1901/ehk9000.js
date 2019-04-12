fe92c30a03139b468f79c87d41f3c98b314c90ea
Erik Konner <erikkonner@gmail.com>
2019-04-12 14:00:40 -0600

    Initial Questions

---
+++ mod-2/memoize/1901/ehk9000.js
@@ -0,0 +1,71 @@
+[
+    {
+        "question": "What string prototype would you use to find the first charecter in this string",
+        "exampleString":  "let lsp = \"Lumpy Space Princes\"",
+        "answer": "lsp.charAt(0)",
+        "category" : "easy",
+        "id": 1
+    },
+    {
+        "queston": "What string prototype would you use to see if \"glob\" is used in this string?",
+        "exampleString": "let  finn = \"Oh my glob!\"",
+        "answer": "finn.includes(\"glob\")",
+        "category": "easy",
+        "id": 2
+    },
+    {
+        "question": "What string prototype would you use to find the first occurance of \"Finn\"?",
+        "exampleString": "let bemo = \" Who wants Finn cakes?\" ",
+        "answer": "bemo.indexOf(\"Finn\")",
+        "category": "easy",
+        "id": 3
+    },
+    {
+        "question": "What string prototype would you use to stop Ice King from yelling?",
+        "exampleString": "let iceKing = \"I JUST WANTED TO BE LOVED!\"",
+        "answer": "iceKing.toLowerCase()",
+        "category": "easy",
+        "id": 4
+    },
+    {
+        "question": "How can we turn what Jake is saying into one variable?",
+        "exampleString": "let jake = \"Dude, suckin at something is the first step\"  let theDog = \"to being sorta good at something\"",
+        "answer": "jake.concat(theDog)",
+        "category": "easy",
+        "id": 5
+    },
+    {
+        "question": "Make Bemo repeat himself three times",
+        "exampleString": "let bemoChop = \"BMO Chop! If this were a real attack, you'd be dead.\"",
+        "answer": "bemoChop.repeat(3)",
+        "category": "easy",
+        "id": 6
+    },
+    {
+        "question": "How would you split each word into its own string?",
+        "exampleString": "let bemo = \"Yes, Finn. It goes in my butt.\"",
+        "answer": "bemo.split(\" \")",
+        "category": "easy",
+        "id": 7
+    },
+    {
+        "question": "What does the string prototype split return?",
+        "answer": "an array",
+        "category": "easy",
+        "id": 8
+    },
+    {
+        "question": "How would you determine how long this string is?",
+        "exampleString": "let finn = \"Jake, it's my special occasion phone! Someone's calling me to tell me about a special occasion.\"",
+        "answer": "finn.length",
+        "category": "easy",
+        "id": 9
+    },
+    {
+        "question" : "How would you make a new string that says \"Finn the Human\" from the provided string?",
+        "exampleString": "let title = \"Jake the dog and Finn the Human\"",
+        "answer": "title.slice(17)",
+        "category": "meduim",
+        "id": 10
+    }
+]
