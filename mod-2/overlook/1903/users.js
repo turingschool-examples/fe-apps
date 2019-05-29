3713831ce32c948c3b64e98007d0a95264d5e5fa
Robbie Jaeger <jaeger.rob@gmail.com>
2019-05-29 10:47:13 -0600

    Change structure of user data

--- mod-2/overlook/1903/users.js
+++ mod-2/overlook/1903/users.js
@@ -1,4 +1,4 @@
-const users = [
+const users = { users: [
   {
     "id": 1,
     "name": "Neil Stoltenberg"
@@ -199,6 +199,6 @@ const users = [
     "id": 50,
     "name": "Burdette Romaguera"
   }
-];
+]};
 
-module.exports = {users};
+module.exports = users;
