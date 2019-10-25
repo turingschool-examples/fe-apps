02985b7d3e54816f451674b85223661063c0c34b
Brittany Storoz <brittanystoroz@gmail.com>
2019-10-25 13:02:43 -0600

    Don't allow dashes

--- index.js
+++ index.js
@@ -93,7 +93,7 @@ datasets.forEach(dataset => {
 
 
 function isValidDate(dateString) {
-  var regEx = /^\d{4}[\/\-](0?[1-9]|1[012])[\/\-](0?[1-9]|[12][0-9]|3[01])$/;
+  var regEx = /^\d{4}[\/](0?[1-9]|1[012])[\/](0?[1-9]|[12][0-9]|3[01])$/;
   return regEx.test(dateString);
 }
 
