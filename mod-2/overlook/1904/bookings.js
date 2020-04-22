15449b1de3d121aef53470b956e36b965c367ff9
Pamela Lovett <pamela.s.lovett@gmail.com>
2020-04-22 07:39:24 -0600

    Update booking for today

--- mod-2/overlook/1904/bookings.js
+++ mod-2/overlook/1904/bookings.js
@@ -2,7 +2,7 @@ const bookings = [
   {
     "id": "5fwrgu4i7k55hl6sz",
     "userID": 9,
-    "date": "2020/02/04",
+    "date": "2020/04/22",
     "roomNumber": 15,
     "roomServiceCharges": []
   },
